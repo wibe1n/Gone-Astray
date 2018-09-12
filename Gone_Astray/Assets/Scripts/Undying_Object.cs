32c6645c7a97f4814fef356329eb2a3f40316ed8
wibe1n <ville.takkinen@outlook.com>
2018-09-12 14:15:08 +0300

    Undying object

---
+++ Gone_Astray/Assets/Scripts/Undying_Object.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Undying_Object : MonoBehaviour {
+
+	
+	void Start () {
+        DontDestroyOnLoad(this);
+        StartCoroutine(ToTheWorld());
+    }
+	
+	private IEnumerator ToTheWorld() {
+        yield return SceneManager.LoadSceneAsync("Level1");
+    }
+
+    public void ExitGame() {
+        StartCoroutine(StartMenuScreen());
+    }
+
+    private IEnumerator StartMenuScreen() {
+        yield return SceneManager.LoadSceneAsync("Menu");
+    }
+
+    public void Tutorial() {
+        StartCoroutine(StartTutorial());
+    }
+
+    private IEnumerator StartTutorial() {
+        yield return SceneManager.LoadSceneAsync("Tutorial");
+    }
+}
