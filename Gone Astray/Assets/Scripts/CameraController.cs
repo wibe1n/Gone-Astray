1611a1dd4bcb7be70350c30921f50850e3f9e31d
Wyrmi <cyan.wyrmie@gmail.com>
2018-09-12 11:28:35 +0300

    Camera&Player controller
    Kuutio (placeholder pelaaja) liikkuu nuoli ja wasd näppäimillä ja kamera liikkuu hiiren mukana

---
+++ Gone Astray/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	//public Camera myCam;
+	public float rotationSpeed; // pelaajaan käännön nopeus
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Käännetään pelaajaa hiiren x-liikkeeellä
+		float mouseInputX = Input.GetAxis("Mouse X");
+		Vector3 lookHereX = new Vector3(0, mouseInputX * rotationSpeed * Time.deltaTime, 0);
+		transform.Rotate(lookHereX);
+
+		// Käännetään asetta hiiren y-liikkeellä
+		float mouseInputY = Input.GetAxis("Mouse Y");
+		Vector3 lookHereY = new Vector3(-1 * mouseInputY * rotationSpeed * Time.deltaTime, 0 , 0);
+		transform.Rotate(lookHereY);
+	}
+}
---
+++ Gone Astray/Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+    public float speed;
+    public float force;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       // if (photonView.isMine)
+        {
+
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f,
+            Input.GetAxis("Vertical") * Time.deltaTime * speed);
+            
+            
+
+        } 
+    }
+}
