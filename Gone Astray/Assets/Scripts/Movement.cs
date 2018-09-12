fbba385c10d8309226e51eee5d3eb3bc9aead595
Wyrmi <cyan.wyrmie@gmail.com>
2018-09-12 14:18:51 +0300

    hide mouse during play
    in inspector the mouse comes back when you press esc

--- Gone Astray/Assets/Scripts/Movement.cs
+++ Gone Astray/Assets/Scripts/Movement.cs
@@ -16,8 +16,10 @@ public class Movement : MonoBehaviour {
 
        // if (photonView.isMine)
         {
-            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f,
-            Input.GetAxis("Vertical") * Time.deltaTime * speed);
+			//transform.position = transform.position + Camera.main.transform.forward;
+			//transform.forward = GameObject.Find("MainCamera").transform.forward;
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+			//transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
         } 
     }
 }
