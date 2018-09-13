ce2f4cb090b15a8162bd0d4f9dd6c9bfd4396ae1
Wyrmi <cyan.wyrmie@gmail.com>
2018-09-13 10:50:09 +0300

    Camera movement locked key pressed
    Camera moves when left ctrl or left mouse button is pressed

--- Gone_Astray/Assets/Scripts/CameraController.cs
+++ Gone_Astray/Assets/Scripts/CameraController.cs
@@ -14,14 +14,16 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Käännetään pelaajaa hiiren x-liikkeeellä
-		float mouseInputX = Input.GetAxis("Mouse X");
-		Vector3 lookHereX = new Vector3(0, mouseInputX * rotationSpeed * Time.deltaTime, 0);
-		transform.Rotate(lookHereX);
+		if (Input.GetAxis ("Fire1") !=0) {
+			// Käännetään pelaajaa hiiren x-liikkeeellä
+			float mouseInputX = Input.GetAxis ("Mouse X");
+			Vector3 lookHereX = new Vector3 (0, mouseInputX * rotationSpeed * Time.deltaTime, 0);
+			transform.Rotate (lookHereX);
 
-		// Käännetään asetta hiiren y-liikkeellä
-		float mouseInputY = Input.GetAxis("Mouse Y");
-		Vector3 lookHereY = new Vector3(-1 * mouseInputY * rotationSpeed * Time.deltaTime, 0 , 0);
-		transform.Rotate(lookHereY);
+			// Käännetään asetta hiiren y-liikkeellä
+			float mouseInputY = Input.GetAxis ("Mouse Y");
+			Vector3 lookHereY = new Vector3 (-1 * mouseInputY * rotationSpeed * Time.deltaTime, 0, 0);
+			transform.Rotate (lookHereY);
+		}
 	}
 }
