e281f115b59f01546614334f3e573e483860e8ce
Wyrmi <cyan.wyrmie@gmail.com>
2018-09-14 10:49:56 +0300

    Menu hahmotelma
    Menu scripti joka loadaa scenen ja sulkee ohjelman napista

---
+++ Gone_Astray/Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnStartGame(){
+		Debug.Log ("start press");
+		SceneManager.LoadScene("Level1");
+		//must add scene to build from File>Build Settings before it can be loaded from here
+	}
+
+	public void OnQuit(){
+		//ei toimi inspectorissa, toimii buildissa
+		Debug.Log ("quit");
+		Application.Quit();
+	}
+}
