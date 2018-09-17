39b2988b23bf1b1df353a0982a0ccc5621ea62f8
wibe1n <ville.takkinen@outlook.com>
2018-09-17 14:16:59 +0300

    objekti interactia
    testicanvasta ja ikonia

---
+++ Gone_Astray/Assets/Scripts/InteractObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractObject : MonoBehaviour
+{
+    public GameObject Canvas;
+
+    void OnTriggerEnter() {
+        Canvas.SetActive(true);
+    }
+    void OnTriggerExit()
+    {
+        Canvas.SetActive(false);
+    }
+}
