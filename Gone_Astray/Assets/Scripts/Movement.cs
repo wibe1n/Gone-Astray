ba0ad4d199a44f9919b1f5629dc3df91c92ca8ce
Wyrmi <cyan.wyrmie@gmail.com>
2018-09-17 11:58:27 +0300

    Hyppy ja kommentointia
    Hyppy ei toimi ellei kappaleella ole rigidbodya

--- Gone_Astray/Assets/Scripts/Movement.cs
+++ Gone_Astray/Assets/Scripts/Movement.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-
-    public float speed;
-	public float rotationSpeed;
-
+	//public muuttujien alkuarvot asetetaan inspectorissa
+    public float speed; //liikkumisnopeus
+	public float rotationSpeed; //kääntymisnopeus
+	Rigidbody rigbod; //rigidbody
+	public float jumpforce;
+	Vector3 jump;
 	// Use this for initialization
 	void Start () {
-		
+		rigbod = gameObject.GetComponent<Rigidbody> ();
+		jump = new Vector3 (0, jumpforce, 0);
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
-		transform.Rotate(0f, Input.GetAxis("Horizontal") * rotationSpeed, 0f);
+	void Update () { //Axis vertical/horizontal tarkoittaa wasd ja nuolinäppäimiä
+        transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed); //hahmo liikkuu eteen ja taakse
+		transform.Rotate(0f, Input.GetAxis("Horizontal") * rotationSpeed, 0f); //hahmo kääntyy vasemmalla ja oikealle
+		if (Input.GetAxis("Jump") != 0 && rigbod.velocity.y == 0){ //jos space painettu ja yn suuntainen nopeus on nolla
+			rigbod.AddForce (jump); //niin hyppää
+		}
     }
 }
