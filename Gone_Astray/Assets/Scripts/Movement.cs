fd36e6f2ecdd786e7cada0f45963b070df06ca81
Wyrmi <cyan.wyrmie@gmail.com>
2018-09-17 13:19:22 +0300

    Hyppy hienosäätö

--- Gone_Astray/Assets/Scripts/Movement.cs
+++ Gone_Astray/Assets/Scripts/Movement.cs
@@ -8,11 +8,13 @@ public class Movement : MonoBehaviour {
 	public float rotationSpeed; //kääntymisnopeus
 	Rigidbody rigbod; //rigidbody
 	public float jumpforce;
-	Vector3 jump;
+	bool onGround = true;
+	bool falling = false;
+
 	// Use this for initialization
 	void Start () {
 		rigbod = gameObject.GetComponent<Rigidbody> ();
-		jump = new Vector3 (0, jumpforce, 0);
+
 
 	}
 	
@@ -20,8 +22,14 @@ public class Movement : MonoBehaviour {
 	void Update () { //Axis vertical/horizontal tarkoittaa wasd ja nuolinäppäimiä
         transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed); //hahmo liikkuu eteen ja taakse
 		transform.Rotate(0f, Input.GetAxis("Horizontal") * rotationSpeed, 0f); //hahmo kääntyy vasemmalla ja oikealle
-		if (Input.GetAxis("Jump") != 0 && rigbod.velocity.y == 0){ //jos space painettu ja yn suuntainen nopeus on nolla
-			rigbod.AddForce (jump); //niin hyppää
+		if (Input.GetAxis("Jump") !=0 && onGround){ //jos space painettu ja yn suuntainen nopeus on nolla
+			rigbod.AddForce (Vector3.up * jumpforce, ForceMode.Impulse); //niin hyppää
+			onGround = false;
+			falling = false;
 		}
+		if (rigbod.velocity.y < 0)
+			falling = true;
+		if (rigbod.velocity.y == 0 && falling)
+			onGround = true;
     }
 }
