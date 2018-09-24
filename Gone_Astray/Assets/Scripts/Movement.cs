13e96b85c05fc72f5340f610aae72f5358371673
wibe1n <ville.takkinen@outlook.com>
2018-09-24 11:38:11 +0300

    testiä

--- Gone_Astray/Assets/Scripts/Movement.cs
+++ Gone_Astray/Assets/Scripts/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
     [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(CapsuleCollider))]
     [RequireComponent(typeof(Animator))]
-    public class ThirdPersonCharacter : MonoBehaviour
+    public class Movement : MonoBehaviour
     {
         [SerializeField] float m_MovingTurnSpeed = 360;
         [SerializeField] float m_StationaryTurnSpeed = 180;
