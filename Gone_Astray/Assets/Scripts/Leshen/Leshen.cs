4a9078a852ff83f0a717509cb28c05631642bb5f
Wyrmi <cyan.wyrmie@gmail.com>
2018-10-04 11:20:10 +0300

    leshen säätö
    kaikki leshenit prefabeina ja ei enää find object with tagiä

--- Gone_Astray/Assets/Scripts/Leshen/Leshen.cs
+++ Gone_Astray/Assets/Scripts/Leshen/Leshen.cs
@@ -6,12 +6,14 @@ public class Leshen : MonoBehaviour {
 
 	//tämä scripti tyhjälle leshen controllerille
 
-	public bool atArea;
+	public bool atArea;					//onko pelaaja trigger arealla
 	public GameObject grownLeshen;		//varsinainen leshen joka kasvaa oikeassa paikassa
 	public GameObject sapling;			//säälittävä pikku taimi joka leshenistä kasvaa random paikassa
 	public GameObject lastLeshen;		//scenessä jo valmiiksi actiivinen leshen
 	public GameObject saplingLocation; //tyhjä objecti joka on pelaajan lapsi ja sijaitsee pelaajan edessä
-	LeshenLerp despawn;
+	public GameObject leshenLocation;	//tyhjä objecti trigger arean vieressä joka saadaan leshenTriggerArea scriptistä
+	public bool leshenActive = false;
+	//LeshenLerp despawn;
 
 	// Use this for initialization
 	void Start () {
@@ -20,21 +22,23 @@ public class Leshen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("7") && !(grownLeshen.activeSelf && atArea)) {
+		if (Input.GetKeyDown ("7") && !(leshenActive && atArea)) { //&& !(grownLeshen.activeSelf && atArea)
 			ActivateLeshen ();
 		}
 	}
 	public void ActivateLeshen(){
-		lastLeshen = GameObject.FindGameObjectWithTag ("Leshen");
+		//lastLeshen = GameObject.FindGameObjectWithTag ("Leshen");
 		if (!(lastLeshen == null)) { 			//vain jos scenessä on jo leshen
 			//despawn = lastLeshen.GetComponent<LeshenLerp> ();
 			//despawn.DespawnLerp ();
-			lastLeshen.SetActive(false);
+			Destroy(lastLeshen);
 		}
 		if (atArea) {
-			grownLeshen.SetActive (true);
+			lastLeshen = Instantiate (grownLeshen, leshenLocation.transform.position, gameObject.transform.rotation);
+			leshenActive = true;
 		} else {
-			Instantiate (sapling, saplingLocation.transform.position, gameObject.transform.rotation);
+			lastLeshen = Instantiate (sapling, saplingLocation.transform.position, gameObject.transform.rotation);
+			leshenActive = false;
 		}	//taimella on scripti joka tuhoaa sen jos setActive on false
 			
 	}
