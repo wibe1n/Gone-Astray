0ba83b200f3f4fe02b5d8c96b018cad50d4f7000
wibe1n <ville.takkinen@outlook.com>
2018-10-04 12:54:47 +0300

    async levelin lataus

---
+++ Gone_Astray/Assets/Scripts/World/LevelDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDestroyer : MonoBehaviour {
+
+    public GameObject previousLevel;
+
+    private void OnTriggerEnter(Collider other) {
+        Destroy(previousLevel);
+    }
+}
---
+++ Gone_Astray/Assets/Scripts/World/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+
+    public string loadPath;
+    private ResourceRequest levelRequest;
+    private GameObject nextLevel;
+
+    private void OnTriggerEnter(Collider col) {
+        AsyncLoadLevel();
+    }
+
+    public void AsyncLoadLevel() {
+        StartCoroutine(AsyncLoad());
+    }
+
+    IEnumerator AsyncLoad() {
+        levelRequest = Resources.LoadAsync(loadPath);
+        yield return levelRequest; 
+        nextLevel = (GameObject)levelRequest.asset;
+        Instantiate(nextLevel);
+    }
+
+}
