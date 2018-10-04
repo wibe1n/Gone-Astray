8ce7f66a7dc0be828b80e66587ac54a82202ca18
Wyrmi <cyan.wyrmie@gmail.com>
2018-10-04 12:55:27 +0300

    sapling.cs hiiteen ja checkpoint

---
+++ Gone_Astray/Assets/Scripts/World/SaveAtCheckpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveAtCheckpoint : MonoBehaviour {
+
+	public Character chara;
+
+	void OnTriggerEnter(Collider player){
+		SaveGame.Instance.playerPosition = transform.position;
+		SaveGame.Instance.fireflies = chara.myFireflies;
+		SaveGame.Instance.fiaFamily = chara.fiaFamily;
+		//todo: levelin tila, collectablet, mätkityt vihut, sidequestit, itemit
+		SaveGame.Save ();
+	}
+}
