7e42f4b2cd31502692c873bd37f68a3173c57d3b
wibe1n <ville.takkinen@outlook.com>
2018-10-08 18:37:01 +0300

    lesheniä muokattu, siivottu kansioita

--- Gone_Astray/Assets/Scripts/Character/Character.cs
+++ Gone_Astray/Assets/Scripts/Character/Character.cs
@@ -6,6 +6,7 @@ public class Character : MonoBehaviour {
 
     public SpeechBubbleCreator speechCreator;
     public CombatController combatController;
+    public GameObject rayCastDrawer, lastSapling, sapling;
     public bool interactableNear = false;
     public bool enemyIsNear = false;
     public bool bossIsNear = false;
@@ -14,18 +15,32 @@ public class Character : MonoBehaviour {
     public bool playerInCombat = false;
     public bool inCombat = false;
     public bool proceed = false;
+    public bool leshenObjectNear = false;
     public NPC myNPC;
     public Enemy myEnemy;
     public List<Firefly> myFireflies = new List<Firefly> { };
     public float stressLevel = 0;
     public List<bool> items = new List<bool> { };
     public List<Firefly> fiaFamily = new List<Firefly> { };
+    float dist = 10;
+    
 
-	void Start () {
-		
-	}
+    void Start () {
+        
+    }
 	
 	void Update () {
+        if (!leshenObjectNear) {
+            if (Input.GetKeyDown("7")) {
+                RaycastHit objectHit;
+                Vector3 down = rayCastDrawer.transform.TransformDirection(Vector3.down);
+                Physics.Raycast(rayCastDrawer.transform.position, down, out objectHit, 50);
+                if (!(lastSapling == null)) {
+                        Destroy(lastSapling);
+                }
+                lastSapling = Instantiate(sapling, objectHit.point, Quaternion.identity);
+            }
+        }
         if (npcIsNear == true) {
             if (Input.GetKeyDown(KeyCode.O)) {
                 if(talking == true) {
---
+++ Gone_Astray/Assets/Scripts/Mechanics/LeshenTriggerArea.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LeshenTriggerArea : MonoBehaviour {
+
+    //tämä scripti sieniympyrälle johon leshen kuuluu istuttaa
+    UnityEvent m_MyEvent = new UnityEvent();
+	public GameObject spawnPosition;
+    public GameObject lastSapling, vegetation;
+    private bool vegetationGrown;
+
+	void OnTriggerEnter(Collider player){
+        m_MyEvent.AddListener(ActivateLeshen);
+        player.GetComponent<Character>().leshenObjectNear = true;
+    }
+	void OnTriggerExit(Collider player){
+        m_MyEvent.RemoveListener(ActivateLeshen);
+        player.GetComponent<Character>().leshenObjectNear = false;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown("7") && m_MyEvent != null) {
+            m_MyEvent.Invoke();
+        }
+    }
+
+    public void ActivateLeshen() {
+        
+        if (!(lastSapling == null)) {           
+            Destroy(lastSapling);
+        }
+        lastSapling = Instantiate(vegetation, spawnPosition.transform.position, gameObject.transform.rotation);
+        vegetationGrown = true;
+
+    }
+}
