efccc1ba6c17a2302a2a5fcb7ba1af6734a99b1f
Wyrmi <cyan.wyrmie@gmail.com>
2018-11-30 12:24:45 +0200

    journalin fireflyt kuntoon

---
+++ Gone_Astray/Assets/Scripts/Menu/FireflyAmount.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireflyAmount : MonoBehaviour {
+
+	public Character chara;
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Text> ().text = "Fireflies: " + chara.myFireflies.Count;
+	}
+}
