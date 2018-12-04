a959b75b1099c63c68a565a56e3ba3c6413d70ea
wibe1n <ville.takkinen@outlook.com>
2018-12-04 22:13:05 +0200

    demoa

--- Gone_Astray/Assets/Scripts/Mechanics/SpoopyTrigger.cs
+++ Gone_Astray/Assets/Scripts/Mechanics/SpoopyTrigger.cs
@@ -7,11 +7,18 @@ public class SpoopyTrigger : MonoBehaviour {
     private FMOD.Studio.EventInstance spoopySounds;
     private FMOD.Studio.ParameterInstance spoopyParameter;
     public PencilContourEffect pencilEffects;
-	
-	void Start () {
+    float currentDarkness, endDarkness;
+
+    float duration;
+    
+
+    void Start () {
         spoopySounds = FMODUnity.RuntimeManager.CreateInstance("event:/Ambience/AmbientMusic");
         spoopySounds.getParameter("Progression", out spoopyParameter);
         Debug.Log(spoopyParameter);
+        currentDarkness = 0;
+        endDarkness = 0.6f;
+        duration = 10f;
     }
 
     private void OnTriggerEnter(Collider player) {
@@ -20,7 +27,22 @@ public class SpoopyTrigger : MonoBehaviour {
             FMODUnity.RuntimeManager.AttachInstanceToGameObject(spoopySounds, player.GetComponent<Transform>(), player.GetComponent<Rigidbody>());
             spoopySounds.start();
             spoopyParameter.setValue(0.2f);
+            StartCoroutine(TurnMusicSpoopy());
             
         }
     }
+
+    public IEnumerator TurnMusicSpoopy()
+    {
+        float timeRemaining = duration;
+        while (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            pencilEffects.m_EdgesOnly = Mathf.Lerp(currentDarkness, endDarkness, Mathf.InverseLerp(duration, 0, timeRemaining));
+            yield return null;
+        }
+        pencilEffects.m_EdgesOnly = endDarkness;
+    }
+
+
 }
