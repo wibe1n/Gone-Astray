f7e4487508912339868adc81f4ec66c78dd01630
Kepsutti <eevis10@hotmail.com>
2018-12-09 10:11:27 +0200

    Cutscene v1

---
+++ Gone_Astray/Assets/Cutscene/CutsceneScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneScript : MonoBehaviour {
+
+    public GameObject mainCamera, cutsceneCamera;
+    public PlayableDirector director;
+    public bool checkTimeline = false;
+
+	// Use this for initialization
+	void Start () {
+        if (cutsceneCamera.activeSelf == true)
+        {
+            cutsceneCamera.SetActive(false);
+        }
+        if (mainCamera.activeSelf == false)
+        {
+            mainCamera.SetActive(true);
+        }
+    }
+	
+    void Update()
+    {
+
+        Debug.Log(director.state);
+
+        if (checkTimeline == true)
+        {
+            if(director.state == PlayState.Paused)
+            {
+                cutsceneCamera.SetActive(false);
+                mainCamera.SetActive(true);
+            }
+        }
+    }
+
+	void OnTriggerEnter(Collider player)
+    {
+        mainCamera.SetActive(false);
+        cutsceneCamera.SetActive(true);
+        director.Play();
+        checkTimeline = true;
+    }
+}
