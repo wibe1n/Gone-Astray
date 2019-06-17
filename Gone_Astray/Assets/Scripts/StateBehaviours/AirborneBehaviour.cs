208516651b37e9d60b0a0b55407d5843054f6976
wibe1n <ville.takkinen@outlook.com>
2019-06-17 14:15:44 +0300

    hyppy ja landaus äänet

---
+++ Gone_Astray/Assets/Scripts/StateBehaviours/AirborneBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirborneBehaviour : StateMachineBehaviour {
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) {
+        FMOD.Studio.EventInstance e = FMODUnity.RuntimeManager.CreateInstance("event:/Character/Movement/Jump");
+        e.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(animator.gameObject.transform.position));
+        e.start();
+        e.release();
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) {
+        FMOD.Studio.EventInstance e = FMODUnity.RuntimeManager.CreateInstance("event:/Character/Movement/Land");
+        e.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(animator.gameObject.transform.position));
+        e.start();
+        e.release();
+
+    }
+
+
+}
