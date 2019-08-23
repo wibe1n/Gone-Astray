98a44e27b55964089381765931dcbb7ec270b20d
wibe1n <ville.takkinen@outlook.com>
2019-08-23 18:48:45 +0300

    alustusta hommille syksyyn

---
+++ Gone_Astray/Assets/Scripts/Visible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visible : MonoBehaviour {
+
+    Renderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        
+    }
+
+    void Update()
+        {
+        if (renderer.isVisible) { }
+        else Destroy(gameObject);
+        }
+    
+}
