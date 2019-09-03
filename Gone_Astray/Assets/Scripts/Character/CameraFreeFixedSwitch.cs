e7a676dd6d5fd97895d961b43b1843e4654f5546
Wyrmi <cyan.wyrmie@gmail.com>
2019-09-03 14:37:17 +0300

    free fix cam

---
+++ Gone_Astray/Assets/Scripts/Character/CameraFreeFixedSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFreeFixedSwitch : MonoBehaviour
+{
+    public bool isFreeCamera = false;
+    public CameraController2 fixCam;
+    public FreeCam freeCam;
+
+    void Start()
+    {
+        if (freeCam == null || fixCam == null)
+            Debug.Log("Camera script not set to free/fix switch");
+        CameraStateUpdate();
+    }
+    public void CameraStateUpdate(){
+        if(isFreeCamera){
+            fixCam.enabled = false;
+            freeCam.enabled = true;
+        }else
+        {
+            fixCam.enabled = true;
+            freeCam.enabled = false;
+        }
+    }
+}
