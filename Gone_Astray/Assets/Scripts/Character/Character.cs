eac7590663ece700e810296a38e74fdd57b96e80
wibe1n <ville.takkinen@outlook.com>
2019-09-09 12:27:20 +0300

    Revert "errors removed"
    This reverts commit 3ceb05d945cb95158623df73b69a0acb6b300bfc.

--- Gone_Astray/Assets/Scripts/Character/Character.cs
+++ Gone_Astray/Assets/Scripts/Character/Character.cs
@@ -67,10 +67,7 @@ public class Character : MonoBehaviour {
         Firefly firefly = new Firefly(0);
         myFireflies.Add(firefly);
         //Päivitetään in game canvasiin kärpästen määrä
-        if(_igcController != null) {
-            _igcController.UpdateFlyAmount(myFireflies.Count);
-        }
-        
+        _igcController.UpdateFlyAmount(myFireflies.Count);
         
         ParticleSystem.MainModule system = fireflies.main;
         if (system.maxParticles != 8) {
