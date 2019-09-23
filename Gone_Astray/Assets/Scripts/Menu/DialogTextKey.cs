1a7378144524ff245b4f02db9635691087ce8aa8
Wyrmi <cyan.wyrmie@gmail.com>
2019-09-23 14:34:03 +0300

    leshen ui

--- Gone_Astray/Assets/Scripts/Menu/DialogTextKey.cs
+++ Gone_Astray/Assets/Scripts/Menu/DialogTextKey.cs
@@ -7,19 +7,38 @@ public class DialogTextKey : MonoBehaviour {
 	
 	public Undying_Object undyObj;
 	public KeyCode talkKey = KeyCode.None;
+    public bool isLeshenIcon = false;
 
 
     
 	// Use this for initialization
 	void Start () {
-		if (GameObject.FindGameObjectWithTag ("UndyingObject") != null) {
-			undyObj = GameObject.FindGameObjectWithTag ("UndyingObject").GetComponent<Undying_Object> ();
-			if (undyObj.talkKey == KeyCode.None)
-				talkKey = KeyCode.E;
-			else
-				talkKey = undyObj.talkKey;
-		}else
-			talkKey = KeyCode.E;
+        if (isLeshenIcon)
+        {
+            if (GameObject.FindGameObjectWithTag("UndyingObject") != null)
+            {
+                undyObj = GameObject.FindGameObjectWithTag("UndyingObject").GetComponent<Undying_Object>();
+                if (undyObj.leshenKey == KeyCode.None)
+                    talkKey = KeyCode.L;
+                else
+                    talkKey = undyObj.leshenKey;
+            }
+            else
+                talkKey = KeyCode.L;
+        }
+        else
+        {
+            if (GameObject.FindGameObjectWithTag("UndyingObject") != null)
+            {
+                undyObj = GameObject.FindGameObjectWithTag("UndyingObject").GetComponent<Undying_Object>();
+                if (undyObj.talkKey == KeyCode.None)
+                    talkKey = KeyCode.E;
+                else
+                    talkKey = undyObj.talkKey;
+            }
+            else
+                talkKey = KeyCode.E;
+        }
 		gameObject.GetComponent<Text> ().text = "Press " + talkKey;
 	}
 }
