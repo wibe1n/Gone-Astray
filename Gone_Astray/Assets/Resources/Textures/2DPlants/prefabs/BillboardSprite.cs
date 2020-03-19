e5f3913c12c0ec3608ef326533c46ccfaff579fa
Noora <noorapan@gmail.com>
2020-03-19 19:39:31 +0200

    kasvidecorations

---
+++ Gone_Astray/Assets/Resources/Textures/2DPlants/prefabs/BillboardSprite.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardSprite : MonoBehaviour
+{
+
+    public Transform MyCameraTransform;
+    private Transform MyTransform;
+    public bool alignNotLook = true; 
+    void Start()
+    {
+        MyTransform = this.transform;
+        MyCameraTransform = Camera.main.transform;
+    }
+    void LateUpdate()
+    {
+        if (alignNotLook)
+            MyTransform.forward = MyCameraTransform.forward;
+        else
+            MyTransform.LookAt(MyCameraTransform, Vector3.up);
+    }
+}
